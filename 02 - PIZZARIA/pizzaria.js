5c42cbe2f6b7d020cc310ea6cc94e4becd280c8b
Kayque <kayquec158@gmail.com>
2025-05-30 16:54:16 -0300

    Meio-Pronto

--- 02 - PIZZARIA/pizzaria.js
+++ 02 - PIZZARIA/pizzaria.js
@@ -1,89 +1,191 @@
-pizzaria = []
+pizzaria = [];
+PizzaParaAlterar = null;
+pedidos = [];
 
 // função para selecionar a operação deseja pelo usuário
 function mostrarSecao(secao) {
+  //Esconde todas as seções
+  document.getElementById("cadastroPizza").classList.add("hidden");
+  document.getElementById("cardapio").classList.add("hidden");
+  document.getElementById("login").classList.add("hidden");
+  document.getElementById("alterar").classList.add("hidden");
+  document.getElementById("compra").classList.add("hidden");
 
-    //Esconde todas as seções
-    document.getElementById("cadastroPizza").classList.add("hidden");
-    document.getElementById("cardapio").classList.add("hidden");
-    document.getElementById("loginCadastro").classList.add("hidden");
-    document.getElementById("login").classList.add("hidden");
-
-    //Exibi apenas a seção que foi selecionada
-    document.getElementById(secao).classList.remove("hidden")
+  //Exibi apenas a seção que foi selecionada
+  document.getElementById(secao).classList.remove("hidden");
 }
 
 //Função para adicionar uma nova pizza ao cardápio
 function cadastrarPizza() {
+  //Buscar Valores definidos pelo usuário
+  const nome = document.getElementById("name").value;
+  const tipo = document.getElementById("type").value;
+  const tamanho = document.getElementById("size").value;
+  const descricao = document.getElementById("description").value;
+  const preco = parseFloat(document.getElementById("price").value);
 
-    //Buscar Valores definidos pelo usuário
-    const nome = document.getElementById("name").value;
-    const tipo = document.getElementById("type").value;
-    const tamanho = document.getElementById("size").value;
-    const descricao = document.getElementById("description").value;
-    const preco = parseFloat(document.getElementById("price").value);
+  //operação para vereficar se todos os campos estão preenchidos
+  if (
+    nome != "" &&
+    tipo != "" &&
+    tamanho != "" &&
+    descricao != "" &&
+    !isNaN(preco)
+  ) {
+    //empurrar as constantes dentro do array Pizzaria
+    pizzaria.push({ nome, tipo, tamanho, descricao, preco });
+    document.getElementById(
+      "result"
+    ).innerHTML = `Pizza adicionada com sucesso!!!`;
 
-    //operação para vereficar se todos os campos estão preenchidos
-    if (nome != "" && tipo != "" && tamanho != "" && descricao != "" && !isNaN(preco)) {
-        //empurrar as constantes dentro do array Pizzaria
-        pizzaria.push({nome, tipo, tamanho, descricao, preco})
-        document.getElementById("result").innerHTML = `Pizza adicionada com sucesso!!!`
-
-        //limpar os termos
-        document.getElementById("name").value = "";
-        document.getElementById("type").value = "";
-        document.getElementById("size").value = "";
-        document.getElementById("description").value = "";
-        document.getElementById("price").value = "";
-        atualizarLista()
-    } else {
-        document.getElementById("result").innerHTML = "Falha em adicionar pizza! Preencha todos os campos antes."
-    }
+    //limpar os termos
+    document.getElementById("name").value = "";
+    document.getElementById("type").value = "";
+    document.getElementById("size").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("price").value = "";
+    atualizarLista();
+  } else {
+    document.getElementById("result").innerHTML =
+      "Falha em adicionar pizza! Preencha todos os campos antes.";
+  }
 }
 
 function buscarPizza() {
-    const busca = document.getElementById("busca").value.toLowerCase();
-    const resultados = pizzaria.filter((pizza) => pizza.nome.toLowerCase().includes(busca.toLowerCase()));
-    atualizarLista(resultados);
+  const busca = document.getElementById("busca").value.toLowerCase();
+  const resultados = pizzaria.filter((pizza) =>
+    pizza.nome.toLowerCase().includes(busca.toLowerCase())
+  );
+  atualizarLista(resultados);
 }
 
-// função para logar usuário/cliente
-function logar() {
+function buscarPizzaAlteracao() {
+  const buscar = document.getElementById("busca-alterar").value.toLowerCase();
 
-    //Buscar valores definidos pelo usuário
-    const user = document.getElementById("user").value;
-    const pass = document.getElementById("password").value;
+  pizzaParaAlterar = pizzaria.find((pizza) =>
+    pizza.nome.toLowerCase().includes(buscar)
+  );
 
-    //Conferir se todos os campos foram preenchidos
-    if (user != "" && pass != "") {
-        //Conferir se o usuário e a senha está correto
-        if(user == "KayqueAdm" && pass == "HanhodeAyds") {
-            document.getElementById("resultadoLogin").innerHTML = `<p style="color: green;">Login realizado com <strong>sucesso!</strong><br>
-            Bem vindo ${user}</p>`;
-        } else {
-            document.getElementById("resultadoLogin").innerHTML = `<p style="color: red;">Usuário ou senha <strong>incorreta!</strong><p>`
-        }
+  if (pizzaParaAlterar) {
+    document.getElementById("form-alterar").classList.remove("hidden");
+    document.getElementById("new-name").value = pizzaParaAlterar.nome;
+    document.getElementById("new-type").value = pizzaParaAlterar.tipo;
+    document.getElementById("new-size").value = pizzaParaAlterar.tamanho;
+    document.getElementById("new-description").value =
+      pizzaParaAlterar.descricao;
+    document.getElementById("new-price").value = pizzaParaAlterar.preco;
+  } else {
+    alert("Pizza não encontrada.");
+  }
+}
+
+function alterarPizza() {
+  if (pizzaParaAlterar) {
+    const novoNome = document.getElementById("new-name").value;
+    const novoTipo = document.getElementById("new-type").value;
+    const novoTamanho = document.getElementById("new-size").value;
+    const novaDescricao = document.getElementById("new-description").value;
+    const novoPreco = parseFloat(document.getElementById("new-price").value);
+
+    if (novoNome && novoTipo && novoTamanho && novaDescricao && novoPreco) {
+      pizzaParaAlterar.nome = novoNome;
+      pizzaParaAlterar.tipo = novoTipo;
+      pizzaParaAlterar.tamanho = novoTamanho;
+      pizzaParaAlterar.descricao = novaDescricao;
+      pizzaParaAlterar.preco = novoPreco;
+
+      atualizarLista();
+      alert("Pizza alterado com sucesso!");
+      document.getElementById("form-alterar").classList.add("hidden");
     } else {
-        document.getElementById("resultadoLogin").innerHTML = `<p>Preencha todos os campos!!!<p>`
+      alert("Por favor, preencha todos os campos.");
     }
-
-
+  }
 }
 
-function atualizarLista(lista = pizzaria) {
-    // define tabela e insere os dados
-    const tabela = document.getElementById("lista-pizzas");
+// função para logar usuário(ADM)
+function logar() {
+  //Buscar valores definidos pelo usuário
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("password").value;
+
+  //Conferir se todos os campos foram preenchidos
+  if (user != "" && pass != "") {
+    //Conferir se o usuário e a senha está correto
+    if (user == "KayqueAdm" && pass == "HanhodeAyds") {
+      document.getElementById(
+        "resultadoLogin"
+      ).innerHTML = `<p style="color: green;">Login realizado com <strong>sucesso!</strong><br>
+            Bem vindo ${user}</p>`;
+    } else {
+      document.getElementById(
+        "resultadoLogin"
+      ).innerHTML = `<p style="color: red;">Usuário ou senha <strong>incorreta!</strong><p>`;
+    }
+  } else {
+    document.getElementById(
+      "resultadoLogin"
+    ).innerHTML = `<p>Preencha todos os campos!!<p>`;
+  }
+}
+
+compras = []
+function comprar(exibir = compras) {
+    const nomeCompra = document.getElementById("name").value;
+    const tipoCompra = document.getElementById("type").value;
+    const tamanhoCompra = document.getElementById("size").value;
+    const descricaoCompra = document.getElementById("description").value;
+    const precoCompra = parseFloat(document.getElementById("price").value);
+
+    compras.push({
+      nomeCompra,
+      tipoCompra,
+      tamanhoCompra,
+      descricaoCompra,
+      precoCompra,
+    });
+    alert("Pizza Salva!");
+
+    const tabela = document.getElementById("exibir-compra");
     tabela.innerHTML = "";
 
-    lista.forEach((pizza) => {
-        const linha = document.createElement("tr");
-        linha.innerHTML = `
+    exibir.forEach((compra) => {
+      const linha = document.createElement("tr");
+      linha.innerHTML = `
+       <td>${compra.nome}</td>
+       <td>${compra.tipo}</td>
+       <td>${compra.tamanho}</td>
+       <td>${compra.descricao}</td>
+       <td>${compra.preco}</td>
+       `;
+      tabela.appendChild(linha);
+    });
+
+}
+ 
+// function pedidosAbertos () {
+//     const nome = document.getElementById('name').value;
+//     const tipo = document.getElementById('type').value;
+//     const tamanho = document.getElementById('size').value;
+//     const descricao =document.getElementById('description').value;
+//     const preco = parseFloat(document.getElementById('price').value);
+// }
+
+function atualizarLista(lista = pizzaria) {
+  // define tabela e insere os dados
+  const tabela = document.getElementById("lista-pizzas");
+  tabela.innerHTML = "";
+
+  lista.forEach((pizza) => {
+    const linha = document.createElement("tr");
+    linha.innerHTML = `
         <td>${pizza.nome}</td>
         <td>${pizza.tipo}</td>
         <td>${pizza.tamanho}</td>
         <td>${pizza.descricao}</td>
         <td>${pizza.preco}</td>
+        <td><button onclick="comprar()">Icone</button></td>
         `;
-        tabela.appendChild(linha);
-    });
+    tabela.appendChild(linha);
+  });
 }
